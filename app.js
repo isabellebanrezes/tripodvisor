4d3dd97ce245732874a79ef594785dd8c3343638
isabelle BANREZES <isabanrezes@gmail.com>
2024-03-08 15:58:59 +0100

    creation fonction et 3 étape pour le formulaire inscription

---
+++ app.js
@@ -0,0 +1,7 @@
+/* #### On veut que le formulaire d'inscription à la newsletter n'apparaisse que si on clique sur le lien "newsletter" dans le menudu haut ########  */
+
+// On va créer une fonction pour réaliser cela :
+
+// Etape 1 : on cible le lien "newsletter" en haut du menu
+// Etape 2 : on pose un écouteur de clic
+// Etape 3 : on affiche la class du formulaire d'inscription
