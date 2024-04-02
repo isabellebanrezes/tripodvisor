3f3230d482b2c8b3677fc2caa3e70dcf7d3c9292
isabelle BANREZES <isabanrezes@gmail.com>
2024-04-02 18:14:46 +0200

    ajout de commentaires

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
+// ############PROJET TRIPODVISOR ################### //
+
 /* #### On veut que le formulaire d'inscription à la newsletter n'apparaisse que si on clique sur le lien "newsletter" dans le menu du haut ########  */
 
 /*################################################################ */
-// On rajoute un "id" unique au niveau du menu newletter "id = menu-newsletter" pour pouvoir le distinguer des autres item du menu et bien le cibler!
+// Dans le fichier HTML, on rajoute un "id" (idendtifiant unique) au niveau du menu newsletter "id = menu-newsletter" pour pouvoir le distinguer des autres item du menu et bien le cibler !
 /*################################################################ */
 
 //------------------------------------------------------------------
-// Etape 1 : on cache le formulaire d'inscription
-// Etape 2 : on pose un ecouteur d'event sur le menu newsletter pour faire apparaitre le formulaire d'inscription
-// Etape 3 : on pose un ecouteur d'event sur la croix du formulaire pour faire disparaitre le formulaire d'inscription
+// Etape 1 : on cache le formulaire d'inscription (on ajoute un class)
+// Etape 2 : on pose un ecouteur d'event sur "newsletter" en haut du menu pour faire apparaitre le formulaire d'inscription
+// Etape 3 : on pose un ecouteur d'event sur la croix du formulaire pour faire disparaitre le formulaire d'inscription à la newsletter
 // Etape 4 : on pose un écouteur d'event quand on scroll à 300px
 // Etape 5 : on veut interdire les adresses emails "fausses" dans le formulaire d'inscription et afficher un message d'erreur
 //------------------------------------------------------------------
 
-// Clic sur Newsletter et l'enveloppe de la balise "a" du menu en haut
+// Clic sur "Newsletter" et l'enveloppe de la balise "a" du menu en haut
 
 // On cible l'id menu-newsletter :
 /* document.getElementById("menu-newsletter").addEventListener("click", (e) => {
@@ -66,10 +68,11 @@ const fordiddenDomains = [
 
 // On cible l'élément choisi : newsletter du menu en haut :
 const newsletterClick = document.getElementById("menu-newsletter");
-// On ajoute un écouteur d'évènement type "click" à notre cible :
+// On ajoute un écouteur d'évènement type "click" (on aurait pu mettre au survol de la souris - mouseover) à notre cible :
 newsletterClick.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("code en détail");
+  console.log("j'appuie sur newsletter en haut du menu");
+  // On fait apparaitre le formulaire en rajoutant la class "newletter--hidden" qui ne cache plus le formulaire
   formNewsletter.classList.remove("newsletter--hidden");
 });
 
@@ -89,14 +92,14 @@ buttonCLoseForm.addEventListener("click", (e) => {
 
 // ----------------           Etape 4      ----------------------------
 
-// On fait apparaitre le formulaire quand on scroll la page d'accueil à 300px
+// On fait apparaitre le formulaire quand on scroll la page d'accueil à 700px
 
 // On pose un écouteur sur la fenêtre pour écouteur l'event "srcoll":
 window.addEventListener("scroll", scrollForm);
 
 // On fait une fonction pour cibler le scroll de la page à 300px :
 function scrollForm(e) {
-  if (window.scrollY > 600) {
+  if (window.scrollY > 700) {
     formNewsletter.classList.remove("newsletter--hidden");
   }
 }
